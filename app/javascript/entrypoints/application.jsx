306a4a386d3fca2497c8ce0ac5ce149d6ceeb62b
joao garcia <jiboiadamor@gmail.com>
2023-06-28 10:46:03 +0100

    started a new rails 7 vite with react jsx for aichat portfolio project

---
+++ app/javascript/entrypoints/application.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../components/App';
+
+console.log('Vite ⚡️ Rails - AIChat');
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App />
+  </>
+);
